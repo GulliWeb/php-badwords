dd018de93754451265f35a8916174f6cfdc27479
Andrea Gulli <andreagulli11@gmail.com>
2024-10-02 17:53:47 +0200

    add call.php file

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-echo "Hello world!"
-?>
+
+?> 
+
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP BADWORDS</title>
+</head>
+<body>
+  <h1>
+   <form action="./call.php" method="GET"></form>
+  </h1>  
+</body>
+</html> 
