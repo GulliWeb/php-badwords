f3f70cd9619a7b016b7f2538d0b101a9fdab5dc7
Andrea Gulli <andreagulli11@gmail.com>
2024-10-02 17:53:53 +0200

    add bootstrap form

--- index.php
+++ index.php
@@ -8,11 +8,28 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Link Bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css">
   <title>PHP BADWORDS</title>
 </head>
 <body>
-  <h1>
-   <form action="./call.php" method="GET"></form>
-  </h1>  
+  <div class="wrapper">
+    <div class="row">
+      <div class="col-12">
+        <h1>
+         <form action="./call.php" method="GET">
+           <div class="mb-3">
+             <label for="exampleFormControlInput1" class="form-label" id="paragraph">Paragrafo</label>
+             <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+             <div class="mb-3">
+               <label for="exampleFormControlTextarea1" class="form-label" id="censured-word">Parola da censurare</label>
+               <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+           </div>
+         </form>
+        </h1>  
+      </div>
+    </div>
+  </div>
 </body>
 </html> 
