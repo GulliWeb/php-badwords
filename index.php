c41fc73761d8c7f67a51b4870a5b41074e272d33
Andrea Gulli <andreagulli11@gmail.com>
2024-10-03 00:29:02 +0200

    improve html layout & add style

--- index.php
+++ index.php
@@ -9,33 +9,29 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Link Bootstrap -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css"> <!-- Link al file CSS -->
   <title>PHP BADWORDS</title>
 </head>
 <body>
   <div class="wrapper">
-    <div class="row">
-      <div class="col-12">
-        <h1>
-         <form action="./call.php" method="GET">
-           <div class="mb-3">
-             <label for="exampleFormControlInput1" class="form-label">Paragrafo</label>
-             <input type="text" class="form-control" name="paragraph" placeholder="Inserisci il paragrafo" required>
-           </div>
-           <div class="mb-3">
-             <label for="exampleFormControlTextarea1" class="form-label">Parola da censurare</label>
-             <textarea class="form-control" name="censored-word" rows="3" placeholder="Inserisci la parola da censurare" required></textarea>
-           </div>
-           <!-- Bottoni per inviare e resettare il form -->
-           <button type="submit" class="btn btn-primary btn-xl">
-            Invia
-           </button>
-           <button type="reset" class="btn btn-warning btn-xl">
-            Reset
-           </button>
-         </form>
-        </h1>  
+    <h1>Gestore di Parole Censurate</h1>
+    <form action="./call.php" method="GET">
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Paragrafo</label>
+        <input type="text" class="form-control" name="paragraph" placeholder="Inserisci il paragrafo" required>
       </div>
-    </div>
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Parola da censurare</label>
+        <textarea class="form-control" name="censored-word" id="censored-word" rows="3" placeholder="Inserisci la parola da censurare" required></textarea>
+      </div>
+      <!-- Bottoni per inviare e resettare il form -->
+      <button type="submit" class="btn btn-primary btn-xl">
+        Invia
+      </button>
+      <button type="reset" class="btn btn-warning btn-xl">
+        Reset
+      </button>
+    </form>
   </div>
 </body>
 </html>
